332f259e4e2267b80c37060a78edccb6d4650e0d
dev global <jsamson257@gmail.com>
2022-01-27 09:49:23 +0800

    change temporarily

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <Router basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route path="/" element={<LoginScreen />} />
+          <Route path="/" element={<RegisterScreen />} />
           <Route path="/register" element={<RegisterScreen />} />
           <Route path="/admin" element={<AdminScreen />} />
           <Route path="/agent" element={<AgentScreen />} />
